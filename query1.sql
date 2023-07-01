ee76fb7e3bb60cf41bae7c4c44f57f33376457bc
martynix <martyna410a@gmail.com>
2023-07-01 21:26:01 +0200

    2

--- query1.sql
+++ query1.sql
@@ -5,3 +5,26 @@ order by 3,4
 select *
 from covid..CovidVaccinations
 order by 3,4
+
+select Location, date, total_cases, new_cases, total_deaths,population
+from covid..CovidDeaths
+order by 1,2
+
+
+
+-- total cases vs total deaths
+-- likelihood of dying if you get sick in Poland
+
+select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+from covid..CovidDeaths
+where location = 'Poland'
+order by 1,2
+
+
+-- total cases vs population
+-- what percentage of population got covid
+
+select Location, date, Population, total_cases, (total_cases/population)*100 as PopulationPercentage
+from covid..CovidDeaths
+where location = 'Poland'
+order by 1,2
